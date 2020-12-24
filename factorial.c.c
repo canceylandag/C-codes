1a019b241238333b4f284ddb61cf82405a64affd
canceylandag <69530509+canceylandag@users.noreply.github.com>
2020-12-24 19:12:24 +0300

    Create factorial.c.c

---
+++ factorial.c.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+/*This is 1st method of taking FActorial.*/
+
+main(){
+	
+	int i,n,factorial=1;
+	char bear;
+
+
+
+
+
+
+	printf("Enter a number.\n");
+	scanf("%d", &n);
+	
+	
+	for(i=1;i<n+1;i++){
+		
+		
+		factorial = i*factorial; 
+		
+		
+	}
+	
+	
+	printf("factorial of %d is %d.\n" ,n,factorial);
+	factorial=1;
+	
+	return 0;
+	
+}
